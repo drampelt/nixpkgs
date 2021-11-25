f708677aa8c4ee3751dfd262fa70ef326d590653
Jonathan Ringer <jonringer117@gmail.com>
2021-11-25 14:46:58 -0800

    python3Packages.luftdaten: 0.6.5 -> 0.7.0

--- pkgs/development/python-modules/luftdaten/default.nix
+++ pkgs/development/python-modules/luftdaten/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "luftdaten";
-  version = "0.6.5";
+  version = "0.7.0";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-5SFb+psULyg9UKVY3oJPNLF3TGS/W+Bxoj79iTzReL4=";
+    sha256 = "0f578c67c96faa05b4bbd0efcd2476c24a0cbb569751c85b72485f4d30301f52";
   };
 
   propagatedBuildInputs = [ aiohttp async-timeout ];
